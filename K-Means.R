cfeb1b003cd680fc93f1c6149b2a8d5b73515293
Jose Tang <jtang68@yahoo.com>
2026-03-17 21:23:02 -0500

    Cambio de Prueba

--- K-Means.R
+++ K-Means.R
@@ -2,3 +2,5 @@
 set.seed(123) # Para que el resultado sea reproducible
 resultado_km <- kmeans(iris[, 1:4], centers = 3)
 print(resultado_km$cluster) # Muestra a qué grupo pertenece cada flor
+
+#Cambio de PRUEBA
